18195d1a4ddd323408082a4adf1f0ff197fab01b
cmsbuild <>
2010-09-22 08:34:49 +0000

    This commit was manufactured by cvs2git to create tag 'GeneratorInterface- Core-CMSSW_3_8_7'.
    Sprout from master 2010-01-22 22:47:34 UTC Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch> 'bring pydata in sync with py6-422'
    Cherrypick from master 2010-09-22 08:34:42 UTC Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch> 'Fix of memory leak for 38X cycle':
        interface/HadronizerFilter.h
    Delete:
        test/BuildFile
        test/EventCounterTester.cc
        test/MockHadronizer.cc
        test/MockHadronizer.h
        test/MockHadronizerGeneratorFilter.cc
        test/TestDriver.cpp
        test/run_MockHadronizerGeneratorFilter.sh
        test/run_MockHadronizerGeneratorFilter_cfg.py

--- interface/HadronizerFilter.h
+++ interface/HadronizerFilter.h
@@ -132,7 +132,7 @@ namespace edm
     //
     if ( decayer_ ) 
     {
-      event.reset( decayer_->decay( event.release() ) );
+      event.reset( decayer_->decay( event.get() ) );
     }
 
     if ( !event.get() ) return false;
